443e421d8560a81a9b205bc297b48c5e2675e02b
Adam <inmon.adam@yahoo.com>
2021-12-12 20:57:10 -0600

    commit tree 4464f016bb8a31679c7399147318df0a3e233adc

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const countDown = 
+      setTimeout(() => {
+        if(timeRemaining == 0){
+          setTimeRemaining(10)
+          onAnswered(false)
+        }
+      else {setTimeRemaining(timeRemaining - 1)}
+    }, 1000);
+
+    return function cleanup(){
+        clearTimeout(countDown) 
+    };
+  })
+
+  // function countDown(){
+   // setInterval(
+ //     (setTimeRemaining(timeRemaining - 1)),1000)
+  // }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
